5b39ef6bdca446d2d2fc4c56e2ba615aafb2f436
hj-noh <hj.noh@samsung.com>
2023-03-10 17:23:51 +0900

    revert http_handle

--- http_handle.py
+++ http_handle.py
@@ -1,24 +1,12 @@
-import ray
+import starlette.requests
 import requests
-from fastapi import FastAPI
 from ray import serve
-import gradio as gr
-from gradio_chatapp import gradio_builder
-
-fast_app = FastAPI()
 
 
-@serve.deployment()
-@serve.ingress(fast_app)
-class App:
-    @fast_app.get("/hello")
-    def hello(self):
-        return "Hello, world!"
+@serve.deployment
+class Counter:
+    def __call__(self, request: starlette.requests.Request):
+        return request.query_params
 
 
-gr.mount_gradio_app(fast_app, gradio_builder(), path="/gradio")
-
-
-app = App.bind()
-
-
+counter = Counter.bind()
