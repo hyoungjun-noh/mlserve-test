1f37a18eb2c93a4985bd97e48284b3d43cefca78
hj-noh <hj.noh@samsung.com>
2023-03-10 15:27:21 +0900

    fast api integration

--- http_handle.py
+++ http_handle.py
@@ -1,12 +1,24 @@
-import starlette.requests
+import ray
 import requests
+from fastapi import FastAPI
 from ray import serve
+import gradio as gr
+from gradio_chatapp import gradio_builder
+
+fast_app = FastAPI()
 
 
-@serve.deployment
-class Counter:
-    def __call__(self, request: starlette.requests.Request):
-        return request.query_params
+@serve.deployment()
+@serve.ingress(fast_app)
+class App:
+    @fast_app.get("/hello")
+    def hello(self):
+        return "Hello, world!"
 
 
-counter = Counter.bind()
+gr.mount_gradio_app(fast_app, gradio_builder(), path="/gradio")
+
+
+app = App.bind()
+
+
